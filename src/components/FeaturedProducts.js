784af9834112fce541e3c690fc81fb5cd05c4c78
Alexa Spalato <alexaspalato@gmail.com>
2022-09-08 12:40:09 +0200

    featured products

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -2,9 +2,41 @@ import React from "react";
 import { storyblokEditable } from "gatsby-source-storyblok";
 
 const FeaturedProducts = ({ blok }) => {
+  const { headline, products } = blok;
+  console.log("blok products", blok);
+  //   const products = blok.product.items;
   return (
-    <div key={blok._uid} {...storyblokEditable(blok)}>
-      FeaturedProducts
+    <div
+      key={blok._uid}
+      {...storyblokEditable(blok)}
+      className="container mx-auto"
+    >
+      <h1 className="mb-5 text-5xl font-bold text-center">{headline}</h1>
+      <div className="grid gap-5 mb-10 md:grid-cols-3">
+        {products.items?.map((product) => {
+          const { description, name, image, variant, id } = product;
+          return (
+            <div className="border product-card">
+              <div key={id}>
+                <div className="product-card__image">
+                  {/* <Image img={media[0].image} /> */}
+                  <img src={image} />
+                </div>
+                <div className="p-4 text-center lowercase product-card__info ">
+                  <h3 className="font-bold">{name}</h3>
+                  {/* <div className="lowercase product-card__price">
+              {price}
+              {currency}
+            </div> */}
+                </div>
+              </div>
+              {/* <div className="flex justify-center mb-5">
+          <ButtonAddToCart variantId={variants[0].shopifyId} className="btn" />
+        </div> */}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "~/components/Layout";
 // import Hero from "~/components/Hero";
-import CategoryGrid from "~/components/CategoryGrid";
+// import CategoryGrid from "~/components/CategoryGrid";
 import { graphql } from "gatsby";
 import {
   useStoryblokState,
@@ -11,7 +11,9 @@ import {
 
 const HomePage = ({ data }) => {
   let story = data.storyblokEntry;
-  story = useStoryblokState(story);
+  story = useStoryblokState(story, {
+    resolveRelations: "featured-products.product",
+  });
   console.log("story", story);
 
   const components = story.content.body?.map((blok) => (
