2e60d83953b88ab4e8245f1510d9c90dad6a9931
Alexa Spalato <alexaspalato@gmail.com>
2022-09-08 17:06:21 +0200

    featured products additional data

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { storyblokEditable } from "gatsby-source-storyblok";
+import { useStaticQuery, Link, graphql } from "gatsby";
+import { ButtonAddToCart } from "~/components/shoppingCart";
+
+const ALL_PRODUCTS_QUERY = graphql`
+  query {
+    allShopifyProduct(limit: 100) {
+      nodes {
+        id
+        handle
+        title
+        priceRangeV2 {
+          maxVariantPrice {
+            amount
+          }
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        variants {
+          shopifyId
+        }
+      }
+    }
+  }
+`;
 
 const FeaturedProducts = ({ blok }) => {
   const { headline, products } = blok;
   console.log("blok products", blok);
-  //   const products = blok.product.items;
+  const data = useStaticQuery(ALL_PRODUCTS_QUERY);
+  const allProducts = data.allShopifyProduct.nodes;
+  console.log("allProducts", allProducts);
   return (
     <div
       key={blok._uid}
@@ -13,26 +41,40 @@ const FeaturedProducts = ({ blok }) => {
     >
       <h1 className="mb-5 text-5xl font-bold text-center">{headline}</h1>
       <div className="grid gap-5 mb-10 md:grid-cols-3">
-        {products.items?.map((product) => {
-          const { description, name, image, variant, id } = product;
+        {products.items?.map((sbProduct) => {
+          const { name, image, id } = sbProduct;
+          const product = allProducts.find((p) => p.title === name);
+          const {
+            handle,
+            priceRangeV2: {
+              maxVariantPrice: { amount: maxPrice },
+              minVariantPrice: { amount: minPrice, currencyCode: currency },
+            },
+            variants,
+          } = product;
+          const price =
+            maxPrice === minPrice ? `${minPrice}` : `from ${minPrice}`;
+          console.log("product", product);
           return (
             <div className="border product-card">
-              <div key={id}>
+              <Link to={`/${handle}`} key={id}>
                 <div className="product-card__image">
-                  {/* <Image img={media[0].image} /> */}
                   <img src={image} />
                 </div>
                 <div className="p-4 text-center lowercase product-card__info ">
                   <h3 className="font-bold">{name}</h3>
-                  {/* <div className="lowercase product-card__price">
-              {price}
-              {currency}
-            </div> */}
+                  <div className="lowercase product-card__price">
+                    {price}
+                    {currency}
+                  </div>
                 </div>
+              </Link>
+              <div className="flex justify-center mb-5">
+                <ButtonAddToCart
+                  variantId={variants[0].shopifyId}
+                  className="btn"
+                />
               </div>
-              {/* <div className="flex justify-center mb-5">
-          <ButtonAddToCart variantId={variants[0].shopifyId} className="btn" />
-        </div> */}
             </div>
           );
         })}
